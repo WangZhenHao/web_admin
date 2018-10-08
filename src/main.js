e2a86504d9677e888a358e42c88f70238a44ea8d
a_boy <1471047476@qq.com>
2018-10-08 15:43:26 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './config/config.js';
+import '../static/js/page.js';
+import '../static/js/webapp.js';
+
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
 // import ElementUI from 'element-ui';
-import './config/config.js';
-import '../static/js/page.js';
-import '../static/js/webapp.js';
 import './beforeRouter.js';
 
 import 'STATIC/bootstrap/css/bootstrap.css';
